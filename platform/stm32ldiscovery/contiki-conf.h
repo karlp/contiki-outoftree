3c318787ef59c0a38e2e16da97a1668591ed064c
Karl Palsson <karlp@tweak.net.au>
2013-05-15 23:32:35 +0000

    Remove support for deprecated types we don't need

--- platform/stm32ldiscovery/contiki-conf.h
+++ platform/stm32ldiscovery/contiki-conf.h
@@ -14,14 +14,6 @@
 
 #define CLOCK_CONF_SECOND 1000
 
-/* These names are deprecated, use C99 names. */
-typedef uint8_t u8_t;
-typedef uint16_t u16_t;
-typedef uint32_t u32_t;
-typedef int8_t s8_t;
-typedef int16_t s16_t;
-typedef int32_t s32_t;
-
 typedef unsigned int clock_time_t;
 typedef unsigned int uip_stats_t;
 
