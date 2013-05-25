95341aec423a8589690ce7614c363617d69fe9dd
Karl Palsson <karlp@tweak.net.au>
2013-05-25 23:26:30 +0000

    Update to latest libopencm3

--- platform/stm32ldiscovery/contiki-main.c
+++ platform/stm32ldiscovery/contiki-main.c
@@ -17,7 +17,7 @@
 static void
 configure_mcu_clocks(void)
 {
-	rcc_clock_setup_pll(&clock_vrange1_config[CLOCK_VRANGE1_HSI_PLL_32MHZ]);
+	rcc_clock_setup_pll(&clock_config[CLOCK_VRANGE1_HSI_PLL_32MHZ]);
 }
 
 int
