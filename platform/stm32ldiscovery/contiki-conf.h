fe57509047841bc1e0a96113e7fb701aa3577e1f
Karl Palsson <karlp@tweak.net.au>
2013-06-20 01:26:00 +0000

    WIP - hacky attempt to copy network init from seedeye
    Crashes on my board, but it's a start!

--- platform/stm32ldiscovery/contiki-conf.h
+++ platform/stm32ldiscovery/contiki-conf.h
@@ -7,6 +7,7 @@
 #define CLIF
 
 #define WITH_UIP 1
+#define WITH_UIP6 1
 #define WITH_ASCII 1
 #define WITH_SERIAL_LINE_INPUT 1
 
@@ -21,13 +22,78 @@ typedef unsigned int uip_stats_t;
 #define BV(x) (1<<(x))
 #endif
 
-/* uIP configuration */
-#define UIP_CONF_LLH_LEN         0
-#define UIP_CONF_BROADCAST       1
-#define UIP_CONF_LOGGING 1
-#define UIP_CONF_BUFFER_SIZE 116
+/// From seedeye
+#define RF_CHANNEL                              12
 
-#define UIP_CONF_TCP_FORWARD 1
+#define PROFILE_CONF_ON 0
+#ifndef ENERGEST_CONF_ON
+#define ENERGEST_CONF_ON 1
+#endif /* ENERGEST_CONF_ON */
+
+#ifdef WITH_UIP6
+#define NETSTACK_CONF_NETWORK                   sicslowpan_driver
+#define NETSTACK_CONF_FRAMER                    framer_802154
+#define NETSTACK_CONF_MAC                       nullmac_driver
+#define NETSTACK_CONF_RDC                       nullrdc_driver
+#define NETSTACK_CONF_RADIO                     mrf24j40_driver
+#define RIMEADDR_CONF_SIZE                      8
+#else
+#define NETSTACK_CONF_NETWORK                   rime_driver
+#define NETSTACK_CONF_FRAMER                    framer_802154
+#define NETSTACK_CONF_MAC                       nullmac_driver
+#define NETSTACK_CONF_RDC                       nullrdc_driver
+#define NETSTACK_CONF_RADIO                     mrf24j40_driver
+#define RIMEADDR_CONF_SIZE                      2
+#endif
+
+
+#ifdef WITH_UIP6
+#define UIP_CONF_ROUTER                         1
+#ifndef UIP_CONF_IPV6_RPL
+#define UIP_CONF_IPV6_RPL                       1
+#endif /* UIP_CONF_IPV6_RPL */
+
+/* IPv6 configuration options */
+#define UIP_CONF_IPV6                           1
+#define UIP_CONF_DS6_NBR_NBU                    20 /* number of neighbors */
+#define UIP_CONF_DS6_ROUTE_NBU                  20 /* number of routes */
+#define UIP_CONF_ND6_SEND_RA                    0
+#define UIP_CONF_ND6_REACHABLE_TIME             600000
+#define UIP_CONF_ND6_RETRANS_TIMER              10000
+
+
+#define UIP_CONF_BUFFER_SIZE                    240
+
+#ifndef QUEUEBUF_CONF_NUM
+#define QUEUEBUF_CONF_NUM                       16
+#endif /* QUEUEBUF_CONF_NUM */
+
+/* UDP configuration options */
+#define UIP_CONF_UDP                            1
+#define UIP_CONF_UDP_CHECKSUMS                  1
+#define UIP_CONF_UDP_CONNS                      10
+
+/* 6lowpan options (for ipv6) */
+#define SICSLOWPAN_CONF_COMPRESSION             SICSLOWPAN_COMPRESSION_HC06
+#define SICSLOWPAN_CONF_MAX_ADDR_CONTEXTS       2
+#define SICSLOWPAN_CONF_COMPRESSION_THRESHOLD   63
+#ifndef SICSLOWPAN_CONF_FRAG
+#define SICSLOWPAN_CONF_FRAG                    1
+#define SICSLOWPAN_CONF_MAXAGE                  8
+#endif /* SICSLOWPAN_CONF_FRAG */
+
+/* General configuration options */
+#define UIP_CONF_STATISTICS                     0
+//#define UIP_CONF_LOGGING                        0
+#define UIP_CONF_LOGGING                        1
+#define UIP_CONF_BROADCAST                      1
+#define UIP_CONF_LLH_LEN                        0
+#define UIP_CONF_LL_802154                      1
+#endif
+
+//// END OF SEEDEYE
+
+//#define UIP_CONF_TCP_FORWARD 1
 
 /* Prefix for relocation sections in ELF files */
 #define REL_SECT_PREFIX ".rel"
--- platform/stm32ldiscovery/contiki-main.c
+++ platform/stm32ldiscovery/contiki-main.c
@@ -10,8 +10,25 @@
 #include <dev/serial-line.h>
 #include <dev/leds.h>
 
+#include <contiki-net.h>
+#include <net/netstack.h>
+#include <net/queuebuf.h>
+#include <net/mac/frame802154.h>
+
+#if WITH_UIP6
+#include "net/uip-ds6.h"
+#endif /* WITH_UIP6 */
+
 #include "contiki-conf.h"
 #include <libopencm3/stm32/rcc.h>
+
+#define DEBUG 1
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
 
 
 static void
@@ -19,6 +36,115 @@ configure_mcu_clocks(void)
 {
 	rcc_clock_setup_pll(&clock_config[CLOCK_VRANGE1_HSI_PLL_32MHZ]);
 }
+
+void
+init_net(uint8_t node_id)
+{
+  uint16_t shortaddr;
+  uint64_t longaddr;
+  rimeaddr_t addr;
+#if WITH_UIP6
+  uip_ds6_addr_t *lladdr;
+  uip_ipaddr_t ipaddr;
+#endif
+  
+  uint8_t i;
+
+  memset(&shortaddr, 0, sizeof(shortaddr));
+  memset(&longaddr, 0, sizeof(longaddr));
+  *((uint8_t *)&shortaddr) = node_id >> 8;
+  *((uint8_t *)&shortaddr + 1) = node_id;
+  *((uint8_t *)&longaddr) = node_id >> 8;
+  *((uint8_t *)&longaddr + 1) = node_id;
+  for(i = 2; i < sizeof(longaddr); ++i) {
+    ((uint8_t *)&longaddr)[i] = random_rand();
+  }
+  
+  PRINTF("SHORT MAC ADDRESS %02x:%02x\n",
+         *((uint8_t *) & shortaddr), *((uint8_t *) & shortaddr + 1));
+
+  PRINTF("EXTENDED MAC ADDRESS %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x\n",
+         *((uint8_t *)&longaddr),
+         *((uint8_t *)&longaddr + 1),
+         *((uint8_t *)&longaddr + 2),
+         *((uint8_t *)&longaddr + 3),
+         *((uint8_t *)&longaddr + 4),
+         *((uint8_t *)&longaddr + 5),
+         *((uint8_t *)&longaddr + 6),
+         *((uint8_t *)&longaddr + 7));
+
+  memset(&addr, 0, sizeof(rimeaddr_t));
+
+  for(i = 0; i < sizeof(addr.u8); ++i) {
+    addr.u8[i] = ((uint8_t *)&longaddr)[i];
+  }
+
+  rimeaddr_set_node_addr(&addr);
+  
+  PRINTF("Rime started with address: ");
+  for(i = 0; i < sizeof(addr.u8) - 1; ++i) {
+    PRINTF("%d.", addr.u8[i]);
+  }
+  PRINTF("%d\n", addr.u8[i]);
+
+  queuebuf_init();
+
+  NETSTACK_RADIO.init();
+  
+  mrf24j40_set_channel(RF_CHANNEL);
+  mrf24j40_set_panid(IEEE802154_PANID);
+  mrf24j40_set_short_mac_addr(shortaddr);
+  mrf24j40_set_extended_mac_addr(longaddr);
+
+  NETSTACK_RDC.init();
+  NETSTACK_MAC.init();
+  NETSTACK_NETWORK.init();
+
+  PRINTF("%s %s, channel check rate %d Hz, radio channel %u\n",
+         NETSTACK_MAC.name, NETSTACK_RDC.name,
+         CLOCK_SECOND / (NETSTACK_RDC.channel_check_interval() == 0 ? 1 :
+                         NETSTACK_RDC.channel_check_interval()), RF_CHANNEL);
+
+#if WITH_UIP6
+
+#if RIMEADDR_CONF_SIZE == 2
+  memset(&uip_lladdr.addr, 0, sizeof(uip_lladdr.addr));
+  uip_lladdr.addr[3] = 0xff;
+  uip_lladdr.addr[4]= 0xfe;
+  memcpy(&uip_lladdr.addr[6], &shortaddr, sizeof(shortaddr));
+#else
+  memcpy(&uip_lladdr.addr, &longaddr, sizeof(uip_lladdr.addr));
+#endif
+
+  process_start(&tcpip_process, NULL);
+
+  lladdr = uip_ds6_get_link_local(-1);
+
+  PRINTF("Tentative link-local IPv6 address ");
+
+  for(i = 0; i < 7; ++i) {
+    PRINTF("%02x%02x:", lladdr->ipaddr.u8[i * 2], lladdr->ipaddr.u8[i * 2 + 1]);
+  }
+
+  PRINTF("%02x%02x\n", lladdr->ipaddr.u8[14], lladdr->ipaddr.u8[15]);
+
+  if(!UIP_CONF_IPV6_RPL) {
+    uip_ip6addr(&ipaddr, 0x2001, 0x1418, 0x100, 0x823c, 0, 0, 0, 0);
+    uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+    uip_ds6_addr_add(&ipaddr, 0, ADDR_TENTATIVE);
+
+    PRINTF("Tentative global IPv6 address ");
+
+    for(i = 0; i < 7; ++i) {
+      PRINTF("%02x%02x:", ipaddr.u8[i * 2], ipaddr.u8[i * 2 + 1]);
+    }
+
+    PRINTF("%02x%02x\n", ipaddr.u8[7 * 2], ipaddr.u8[7 * 2 + 1]);
+  }
+#endif
+}
+
+
 
 int
 main()
@@ -30,6 +156,7 @@ main()
   clock_init();
   rtimer_init();
   leds_init();
+  init_net(0x88);
   process_init();
 #if WITH_SERIAL_LINE_INPUT
   uart_set_input(serial_line_input_byte);
