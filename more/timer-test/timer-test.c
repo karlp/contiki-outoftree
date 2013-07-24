5efe68c196b166921f2f1a1ea4f181aaeda8de17
Karl Palsson <karlp@tweak.net.au>
2013-07-24 20:15:38 +0000

    [timer-tests] Make all tests run
    Can't figure out how to make contiki run a set of processes one by one, and
    it's not the aim of my current project, so just run them all in a row inside
    one big test process.

--- more/timer-test/timer-test.c
+++ more/timer-test/timer-test.c
@@ -37,88 +37,40 @@
 
 #include <stdio.h>
 
-PROCESS(test_etimer, "etimer and ctimer tests");
-PROCESS(test_clock_usecs, "clock low level routine tests");
-PROCESS(test_clock_wait, "clock low level routine tests2");
-PROCESS(foo_process, "test runner process");
-AUTOSTART_PROCESSES(&foo_process);
+PROCESS(tests, "timer and clock test routines");
+AUTOSTART_PROCESSES(&tests);
 
-
-PROCESS_THREAD(test_etimer, ev, data)
+PROCESS_THREAD(tests, ev, data)
 {
-	static int tick_count = 0;
+	static int tick_count = 10;
 	static int ticks = 0;
-	PROCESS_BEGIN();
-	tick_count = *(int *) data;
-	printf("TESTSTART{\"expected\":%d}\n", tick_count * 100);
-
+	int i, k;
 	static struct etimer et; // Define the timer
+	PROCESS_BEGIN();
 
+
+	printf("TESTSTART{\"expected\":%d}\n", tick_count * 100);
 	etimer_set(&et, 100);
-	for (; ticks < tick_count; ticks++) {
+	for (ticks = 0; ticks < tick_count; ticks++) {
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 		printf("etimer tick %d/%d\n", ticks, tick_count);
 		etimer_reset(&et);
 	}
-
 	printf("TESTEND\n");
-	PROCESS_END();
-}
 
-PROCESS_THREAD(test_clock_usecs, ev, data)
-{
-	static int tick_count = 0;
-	static int ticks = 0;
-	int k;
-	PROCESS_BEGIN();
-	tick_count = *(int *) data;
+
 	printf("TESTSTART{\"expected\":%d}\n", tick_count * 100);
-
-	for (; ticks < tick_count; ticks++) {
-		for (k = 0; k < 100; k++) {
+	for (ticks = 0; ticks < tick_count; ticks++) {
+		for (i = 0; i < 100; i++) {
 			clock_delay_usec(1000);
 		}
 		printf("clock_delay_usec: 100 * 1000usecs, %d/%d\n", ticks, tick_count);
 	}
-
 	printf("TESTEND\n");
-	PROCESS_END();
-}
 
-PROCESS_THREAD(test_clock_wait, ev, data)
-{
-	PROCESS_BEGIN();
 	printf("TESTSTART{\"expected\":5000}\n");
 	clock_wait(5 * CLOCK_SECOND);
 	printf("TESTEND\n");
-	PROCESS_END();
-}
-
-void
-test_etimer_init(int i)
-{
-	process_start(&test_etimer, (void *) &i);
-}
-
-void
-test_clock_usecs_init(int i)
-{
-	process_start(&test_clock_usecs, (void *) &i);
-}
-
-
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(foo_process, ev, data)
-{
-	PROCESS_BEGIN();
-
-	printf("Hello foo world\n");
-	test_clock_usecs_init(10);
-	PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_EXITED);
-	test_etimer_init(10);
-	PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_EXITED);
-	process_start(&test_clock_wait, NULL);
-	PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_EXITED);
 
 	PROCESS_END();
 }
